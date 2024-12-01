a5cf1171e577e68b03b74d107d1f6f52ad69ccda
Junfeng Lei <junf137@outlook.com>
2024-11-30 23:38:50 -0500

    [feat] remove unnecessary code for 2D CNN part

--- code/host-starter-solution.py
+++ code/host-starter-solution.py
@@ -295,19 +295,7 @@ print(np.mean(100 * np.abs(residuals / valid_targets_wc)))
 # np.save(f"{output_dir}/std_valid_wc.npy", spectre_valid_std_wc)
 
 
-# %% [markdown]
-# # 2D CNN for atmospheric features
-#
-# <a id="fluctu"></a>
-# We now remove the mean value (transit depth) of the spectra to keep the atmospheric features only
-
-# %% [markdown]
-# ## Preprocessing for 2D CNN
-#
-# Centers each target by subtracting its mean value, isolating variations around the mean.
-
-
-# %%
+# %% 2D CNN for atmospheric features
 def suppress_mean(targets, mean):
     """
     Suppress the mean of the targets along the columns.
@@ -329,16 +317,8 @@ valid_targets_shift = suppress_mean(valid_targets, targets_mean[~list_index_trai
 # train_targets_shift.shape (538, 283)
 # valid_targets_shift.shape (135, 283)
 
-print("targets_mean.shape", targets_mean.shape)
-print("train_targets_shift.shape", train_targets_shift.shape)
-print("valid_targets_shift.shape", valid_targets_shift.shape)
 
-# %% [markdown]
-# We normalize the targets so that they range between -1 and 1, centered on zero
-
-
-# %%
-##### normalization of the targets ###
+# normalization of the targets
 def targets_normalization(data1, data2):
     """
     Normalize the targets by scaling them into the range [0, 1].
@@ -355,37 +335,12 @@ def targets_normalization(data1, data2):
 
 train_targets_norm, valid_targets_norm, targets_abs_max = targets_normalization(train_targets_shift, valid_targets_shift)
 
-print("train_targets_norm.shape", train_targets_norm.shape)
-print("valid_targets_norm.shape", valid_targets_norm.shape)
-
-# %%
-plt.figure(figsize=(15, 5))
-
-for i in range(240):
-    plt.plot(wls, train_targets_norm[i], "g-", alpha=0.5)
-plt.plot([], [], "g-", alpha=0.5, label="Train targets")
-
-for i in range(60):
-    plt.plot(wls, valid_targets_norm[i], "r-", alpha=0.7)
-plt.plot([], [], "r-", alpha=0.5, label="Valid targets (true mean)")  # TODO: what is this?
-
-plt.legend()
-plt.ylabel(f"$(R_p/R_s)^2$")
-plt.title("All targets after subtracting the mean value and normalization")
-plt.show()
-
-# %%
-###### Transpose #####
+# Transpose
 train_obs = train_obs.transpose(0, 2, 1)
 valid_obs = valid_obs.transpose(0, 2, 1)
-print(train_obs.shape)
-
-# %% [markdown]
-# We cut the transit to keep the in-transit. We assume an arbitrary transit duration of 40 instants with a transit occuring between 75 and 115.
 
 
-# %%
-##### Subtracting the out transit signal #####
+# Subtracting the out transit signal
 def suppress_out_transit(data, ingress, egress):
     data_in = data[:, ingress:egress, :]
     return data_in
@@ -395,15 +350,8 @@ ingress, egress = 75, 115
 train_obs_in = suppress_out_transit(train_obs, ingress, egress)
 valid_obs_in = suppress_out_transit(valid_obs, ingress, egress)
 
-print("train_obs_in.shape", train_obs_in.shape)
-print("valid_obs_in.shape", valid_obs_in.shape)
 
-# %% [markdown]
-# We remove the mean value of the in-transit to get relative data like the targets
-
-
-# %%
-###### Subtract the mean #####
+# Subtract the mean
 def subtract_data_mean(data):
     data_mean = np.zeros(data.shape)
     for i in range(data.shape[0]):
@@ -414,115 +362,12 @@ def subtract_data_mean(data):
 train_obs_2d_mean = subtract_data_mean(train_obs_in)
 valid_obs_2d_mean = subtract_data_mean(valid_obs_in)
 
-print("train_obs_2d_mean.shape", train_obs_2d_mean.shape)
-print("valid_obs_2d_mean.shape", valid_obs_2d_mean.shape)
-
-# %% [markdown]
-# We use the same normalization as for the targets, i.e. between -1 and 1 centered on zero
-
-# %%
-##### Normalization dataset #####
+# Normalization dataset
 data_normalization = targets_normalization
-
-
-# TODO: this function is not used
-def data_normback(data, data_abs_max):
-    return data * data_abs_max
 
 
 train_obs_norm, valid_obs_norm, data_abs_max = data_normalization(train_obs_2d_mean, valid_obs_2d_mean)
 
-print("train_obs_norm.shape", train_obs_norm.shape)
-print("valid_obs_norm.shape", valid_obs_norm.shape)
-
-# %%
-plt.figure(figsize=(15, 5))
-
-for i in range(train_obs.shape[0]):
-    plt.plot(wls, train_obs_norm[i, 10], "g-", alpha=0.5)
-plt.plot([], [], "g-", alpha=0.5, label="Train targets")
-
-for i in range(valid_obs.shape[0]):
-    plt.plot(wls, valid_obs_norm[i, 10], "r-", alpha=0.7)
-plt.plot([], [], "r-", alpha=0.5, label="Valid targets (true mean)")
-
-plt.legend()
-plt.ylabel(f"$(R_p/R_s)^2$")
-plt.title("Train and Valid data after subtracting the mean value and normalization")
-plt.show()
-
-# %% [markdown]
-# ## Train 2D CNN
-
-
-# %%
-class CNN2DModel(nn.Module):
-    def __init__(self):
-        super(CNN2DModel, self).__init__()
-
-        # Define the convolutional layers
-        self.conv_2d_1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 1), padding="same")
-        self.conv_2d_2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 1), padding="same")
-        self.conv_2d_3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 1), padding="same")
-        self.conv_2d_4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 1), padding="same")
-        self.conv_2d_5 = nn.Conv2d(in_channels=256, out_channels=32, kernel_size=(1, 3), padding="same")
-        self.conv_2d_6 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 3), padding="same")
-        self.conv_2d_7 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(1, 3), padding="same")
-        self.conv_2d_8 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(1, 3), padding="same")
-
-        # Define pooling and batch normalization layers
-        self.pool_2d_1 = nn.MaxPool2d(kernel_size=(2, 1))
-        self.pool_2d_2 = nn.MaxPool2d(kernel_size=(1, 2))
-        self.bn_2d_1 = nn.BatchNorm2d(num_features=32)
-        self.bn_2d_2 = nn.BatchNorm2d(num_features=32)
-
-        self.flatten = nn.Flatten()
-
-        # Define fully connected layers
-        self.fc_1 = nn.Linear(in_features=256 * 5 * 17, out_features=700)
-        self.fc_2 = nn.Linear(in_features=700, out_features=283)
-
-        # Dropout
-        self.dropout = nn.Dropout(0.2)
-
-    def forward(self, x):
-        x = F.relu(self.conv_2d_1(x))
-        x = self.pool_2d_1(x)
-        x = self.bn_2d_1(x)
-
-        x = F.relu(self.conv_2d_2(x))
-        x = self.pool_2d_1(x)
-
-        x = F.relu(self.conv_2d_3(x))
-        x = self.pool_2d_1(x)
-
-        x = F.relu(self.conv_2d_4(x))
-
-        x = F.relu(self.conv_2d_5(x))
-        x = self.pool_2d_2(x)
-        x = self.bn_2d_2(x)
-
-        x = F.relu(self.conv_2d_6(x))
-        x = self.pool_2d_2(x)
-
-        x = F.relu(self.conv_2d_7(x))
-        x = self.pool_2d_2(x)
-
-        x = F.relu(self.conv_2d_8(x))
-        x = self.pool_2d_2(x)
-
-        x = self.flatten(x)
-
-        x = F.relu(self.fc_1(x))
-        x = self.dropout(x)
-
-        x = self.fc_2(x)
-
-        return x
-
-
-# %%
-# define 2D model hyperparameters
 num_epochs_2D = 200
 batch_size_2D = 32
 
@@ -533,20 +378,6 @@ criterion_2D = nn.MSELoss()
 optimizer_2D = optim.Adam(model_2D.parameters(), lr=0.001)
 
 best_valid_loss_2D = float("inf")
-
-# compare pytorch data with Tensorflow data
-compare_tf_data_with_current_data(train_obs_norm, np.load("../output/data_tf/train_obs_norm.npy"), "train_obs_norm")
-compare_tf_data_with_current_data(
-    train_targets_norm,
-    np.load("../output/data_tf/train_targets_norm.npy"),
-    "train_targets_norm",
-)
-compare_tf_data_with_current_data(valid_obs_norm, np.load("../output/data_tf/valid_obs_norm.npy"), "valid_obs_norm")
-compare_tf_data_with_current_data(
-    valid_targets_norm,
-    np.load("../output/data_tf/valid_targets_norm.npy"),
-    "valid_targets_norm",
-)
 
 # Dataset and Dataloader
 train_dataset_2D = TensorDataset(
@@ -560,11 +391,6 @@ valid_dataset_2D = TensorDataset(
 
 train_loader_2D = DataLoader(train_dataset_2D, batch_size=batch_size_2D, shuffle=True)
 valid_loader_2D = DataLoader(valid_dataset_2D, batch_size=batch_size_2D, shuffle=False)
-
-print("train input: ", train_loader_2D.dataset.tensors[0].shape)
-print("train output: ", train_loader_2D.dataset.tensors[1].shape)
-print("valid input: ", valid_loader_2D.dataset.tensors[0].shape)
-print("valid output: ", valid_loader_2D.dataset.tensors[1].shape)
 
 summary(model_2D, input_size=(train_loader_2D.dataset.tensors[0].shape))
 
@@ -601,13 +427,7 @@ print("validation loss 2D: ", max(valid_losses_2D), min(valid_losses_2D))
 # %%
 model_2D, optimizer_2D, num_epochs_2D, best_valid_loss_2D = load_checkpoint(output_dir + "/2D", model_2D, optimizer_2D)
 
-# %% [markdown]
-# ## Postprocessing and visualisation
-
-# %% [markdown]
-# We obtain uncertainties on the predictions by computing a MCDropout.
-
-# %%
+# %% 2D CNN Inference
 nb_dropout = 5
 
 
@@ -639,104 +459,21 @@ def NN_uncertainty(model, x_test, targets_abs_max, nb_dropout, device):
 if do_the_mcdropout:
     spectre_valid_shift, spectre_valid_shift_std = NN_uncertainty(model_2D, valid_loader_2D, targets_abs_max, nb_dropout, device)
 
-# %%
 residuals = valid_targets_shift - spectre_valid_shift
 print("MSE : ", np.sqrt((residuals**2).mean()) * 1e6, "ppm")
 
-# %%
 # np.save(f'{output_dir}/pred_valid_shift.npy', spectre_valid_shift)
 # np.save(f'{output_dir}/targ_valid_shift.npy', valid_targets_shift)
 # np.save(f'{output_dir}/std_valid_shift.npy', spectre_valid_shift_std)
 
-# %%
-plt.figure()
-for i in range(50):
-    plt.plot(spectre_valid_shift[-i] + 0.0001 * i, "-", alpha=0.5)
-plt.title("Spectra predictions for the validation set")
-plt.xlabel("Time")
-plt.ylabel("Arbitrary flux")
-plt.show()
 
-# %%
-list_valid_planets = [0, 12, 35, 60, 70]
-wavelength = np.loadtxt("../dataset/ariel-data-challenge-2024/wavelengths.csv", skiprows=1, delimiter=",")
-uncertainty = spectre_valid_shift_std
-for i in list_valid_planets:
-    plt.figure()
-    plt.title("Result for the sample {} of the validation set".format(i))
-    plt.plot(wavelength, spectre_valid_shift[i], ".k", label="Prediction")
-    plt.plot(wavelength, valid_targets_shift[i], color="tomato", label="Target")
-    plt.fill_between(
-        wavelength,
-        spectre_valid_shift[i] - spectre_valid_shift_std[i],
-        spectre_valid_shift[i] + spectre_valid_shift_std[i],
-        color="silver",
-        alpha=0.8,
-        label="Uncertainty",
-    )
-    plt.legend()
-    plt.ylabel(f"$(R_p/R_s)^2$")
-    plt.xlabel(f"Wavelength ($mu$m)")
-    plt.show()
-
-# %% [markdown]
-# # Combine 1D and 2D CNN output for FINAL SPECTRA
-
-
-# %%
-######## ADD THE FLUCTUATIONS TO THE MEAN ########
-def add_the_mean(shift, mean):
-    return shift + mean[:, np.newaxis]
-
-
-predictions_valid = add_the_mean(spectre_valid_shift, spectre_valid_wc)
+# %% Combine 1D and 2D CNN output for FINAL SPECTRA
+# ADD THE FLUCTUATIONS TO THE MEAN
+predictions_valid = spectre_valid_shift + spectre_valid_wc[:, np.newaxis]
 
 predictions_std_valid = np.sqrt(spectre_valid_std_wc[:, np.newaxis] ** 2 + spectre_valid_shift_std**2)
 
-# %%
-uncertainty = predictions_std_valid
-
-
-def plot_one_sample_valid(ax, p):
-    ax.set_title(f"Result for sample {p} ")
-    (line1,) = ax.plot(wavelength, predictions_valid[p], ".k", label="Prediction")
-    (line2,) = ax.plot(wavelength, valid_targets[p], color="tomato", label="Target")
-    ax.fill_between(
-        wavelength,
-        predictions_valid[p, :] - uncertainty[p],
-        predictions_valid[p, :] + uncertainty[p],
-        color="silver",
-        alpha=0.8,
-        label="Uncertainty",
-    )
-    ax.set_ylabel(f"$(R_p/R_s)^2$")
-    ax.set_xlabel(f"Wavelength ($mu$m)")
-    return line1, line2
-
-
-num_samples = 16
-rows, cols = 4, 4
-
-fig, axs = plt.subplots(rows, cols, figsize=(15, 10))
-samples = [1, 2, 7, 15, 20, 25, 30, 35, 40, 45, 50, 55, 6, 5, 8, 9]
-lines = []
-
-for i, ax in enumerate(axs.flat):
-    lines.extend(plot_one_sample_valid(ax, samples[i]))
-
-fig.legend(
-    lines[:2],
-    ["Prediction", "Target"],
-    loc="upper center",
-    ncol=3,
-    bbox_to_anchor=(0.5, -0.05),
-)
-fig.suptitle("Validation dataset")
-plt.tight_layout()
-plt.show()
-
-# %%
-######## PLOTS THE RESULT ########
+# final spectra and residuals
 predictions = predictions_valid
 targets_plot = valid_targets
 std = predictions_std_valid
@@ -747,46 +484,7 @@ targets_concatenated_plot = np.concatenate(targets_plot, axis=0)
 spectre_valid_std_concatenated = np.concatenate(std, axis=0)
 residuals = targets_concatenated_plot - predictions_concatenated_plot
 uncertainty = spectre_valid_std_concatenated
-
-fig, axs = plt.subplots(2, 1, figsize=(9, 8), gridspec_kw={"height_ratios": [3, 1]})
-
-
-axs[0].plot(wls_concatenated, predictions_concatenated_plot, "-", color="k", label="Prediction")
-axs[0].plot(wls_concatenated, targets_concatenated_plot, "-", color="tomato", label="Target")
-axs[0].fill_between(
-    np.arange(len(wls_concatenated)),
-    predictions_concatenated_plot - uncertainty,
-    predictions_concatenated_plot + uncertainty,
-    color="silver",
-    alpha=1,
-    label="Uncertainty",
-)
-axs[0].set_xlabel("Concatenated wavelengths for all planets")
-axs[0].set_ylabel(f"$(R_p/R_s)^2$")
-axs[0].set_title("Prediction vs target, validation dataset")
-axs[0].legend()
-
-axs[1].plot(wls_concatenated, residuals, "-", color="cornflowerblue", label="Residual")
-axs[1].fill_between(
-    np.arange(len(wls_concatenated)),
-    residuals - uncertainty,
-    residuals + uncertainty,
-    color="lightblue",
-    alpha=0.9,
-    label="Uncertainty",
-)
-axs[1].set_xlabel("Concatenated wavelengths for all planets")
-axs[1].set_ylabel("Residual")
-axs[1].set_title("Residuals with Uncertainty")
-axs[1].legend()
-
-plt.tight_layout()
-plt.show()
-
 print("MSE : ", np.sqrt((residuals**2).mean()) * 1e6, "ppm")
 
-# %%
 # np.save(f'{output_dir}/pred_valid.npy', predictions_valid)
 # np.save(f'{output_dir}/std_valid.npy', predictions_std_valid)
-
-# %%
